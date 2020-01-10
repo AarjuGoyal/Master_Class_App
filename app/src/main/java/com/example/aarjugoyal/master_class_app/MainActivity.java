3a0094f859a1673334055264ebf62847cc8c6132
Saumya Goyal <saumyagoyal01@gmail.com>
2020-01-10 10:44:20 +0530

    Add colors and toolbars

--- app/src/main/java/com/example/aarjugoyal/master_class_app/MainActivity.java
+++ app/src/main/java/com/example/aarjugoyal/master_class_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.aarjugoyal.master_class_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Welcome");
 
         addListenerOnButton1();
 
